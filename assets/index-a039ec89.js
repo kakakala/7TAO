7fc61c8104a3508a74fc7bac06049e1028f564a9
kakakala <8319gary@gmail.com>
2023-08-31 19:24:56 +0800

    Updates

--- assets/index-a039ec89.js
+++
@@ -1,1 +0,0 @@
-import"./main-bf9d39df.js";import e from"https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.mjs";new e(".swiper",{allowTouchMove:!1,slidesPerView:"auto",speed:5e3,loop:!0,autoplay:{delay:0,disableOnInteraction:!1}});
